07d174039112eff36b8ebad47a37e40d623b584d
Grandmaster <grandmaster@ethereal-ai.com>
2021-12-29 14:51:51 +0800

    feat: Add sparse_dot_product function

--- xlr8/linalg.py
+++ xlr8/linalg.py
@@ -2,4 +2,43 @@ import numpy as np
 from scipy.sparse import issparse
 
 def sparse_dot_product(a, b, *, dense_output=False):
-    pass
+    """Dot product that handle the sparse matrix case correctly.
+    Parameters
+    ----------
+    a : {ndarray, sparse matrix}
+    b : {ndarray, sparse matrix}
+    dense_output : bool, default=False
+        When False, ``a`` and ``b`` both being sparse will yield sparse output.
+        When True, output will always be a dense array.
+    Returns
+    -------
+    dot_product : {ndarray, sparse matrix}
+        Sparse if ``a`` and ``b`` are sparse and ``dense_output=False``.
+    """
+    if a.ndim > 2 or b.ndim > 2:
+        if issparse(a):
+            # sparse is always 2D. Implies b is 3D+
+            # [i, j] @ [k, ..., l, m, n] -> [i, k, ..., l, n]
+            b_ = np.rollaxis(b, -2)
+            b_2d = b_.reshape((b.shape[-2], -1))
+            a_b = a @ b_2d
+            a_b = a_b.reshape(a.shape[0], *b_.shape[1:])
+        elif issparse(b):
+            # sparse is always 2D. Implies a is 3D+
+            # [k, ..., l, m] @ [i, j] -> [k, ..., l, j]
+            a_2d = a.reshape(-1, a.shape[-1])
+            a_b = a_2d @ b
+            a_b = a_b.reshape(*a.shape[:-1], b.shape[1])
+        else:
+            a_b = np.dot(a, b)
+    else:
+        a_b = a @ b
+
+    if (
+        issparse(a)
+        and issparse(b)
+        and dense_output
+        and hasattr(a_b, "toarray")
+    ):
+        return a_b.toarray()
+    return a_b
